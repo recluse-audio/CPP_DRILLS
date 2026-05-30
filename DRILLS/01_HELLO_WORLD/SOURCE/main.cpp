7d9a9373911d9bfdecae4eeeb1a83dc949865a81
Ryan Devens <ryan_devens@proton.me>
2026-05-30 14:27:03 -0500

    reorg of files

--- DRILLS/01_HELLO_WORLD/SOURCE/main.cpp
+++ DRILLS/01_HELLO_WORLD/SOURCE/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include "string.h"
+#include "Logic.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "hello world\n";
+    if(argc != 2)
+    {
+        std::cerr << "usage " << argv[0] << " <number>\n";
+        return 1;
+    }
+    int n = std::stoi(argv[1]);
+
+    std::cout << n << " is " << classify[n] << " <number>\n";
+    std::cout << "prime: " << (is_prime(n) ? "yes" : "no") << "\n";
+    std::cout << "binary: " << to_binary(n) << "\n";
+
+
     return 0;
 }
